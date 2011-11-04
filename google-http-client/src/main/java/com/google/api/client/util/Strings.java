4aea4a0f4446f893f27175d250ab517a8a30083f
Ravi Mistry <rmistry@google.com>
2011-11-04 10:45:55 -0700

    start 1.6.1-beta

--- google-http-client/src/main/java/com/google/api/client/util/Strings.java
+++ google-http-client/src/main/java/com/google/api/client/util/Strings.java
@@ -29,7 +29,7 @@ public class Strings {
    *
    * @since 1.3
    */
-  public static final String VERSION = "1.6.0-beta";
+  public static final String VERSION = "1.6.1-beta-SNAPSHOT";
 
   /**
    * Line separator to use for this OS, i.e. {@code "\n"} or {@code "\r\n"}.
