d5d71b5ebbd14dd6ccf8c14e2b740c6eb7c62d0a
Matthias Linder <mlinder@google.com>
2012-04-26 16:49:19 -0400

    [OAuth Issue 34] Testing interface for System.currentTimeMillis() http://codereview.appspot.com/6109055/

---
+++ google-http-client/src/main/java/com/google/api/client/testing/http/FixedClock.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.api.client.testing.http;
+
+import com.google.api.client.util.Clock;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * A thread-safe fixed time implementation of the Clock to be used for unit testing.
+ *
+ * <p>
+ * Explicitly allows you to set the time to any arbitrary value.
+ * </p>
+ *
+ * @since 1.9
+ * @author mlinder@google.com (Matthias Linder)
+ */
+public class FixedClock implements Clock {
+  private AtomicLong currentTime;
+
+  /**
+   * Initializes the FixedClock with 0 millis as start time.
+   */
+  public FixedClock() {
+    this(0L);
+  }
+
+  /**
+   * Initializes the FixedClock with the specified time.
+   * @param startTime time in milliseconds used for initialization.
+   */
+  public FixedClock(long startTime) {
+    currentTime = new AtomicLong(startTime);
+  }
+
+  /**
+   * Changes the time value this time provider is returning.
+   * @param newTime New time in milliseconds.
+   */
+  public FixedClock setTime(long newTime) {
+    currentTime.set(newTime);
+    return this;
+  }
+
+  public long currentTimeMillis() {
+    return currentTime.get();
+  }
+}
---
+++ google-http-client/src/main/java/com/google/api/client/util/Clock.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.api.client.util;
+
+/**
+ * Clock which can be used to get the amount of elapsed milliseconds in system time.
+ *
+ * <p>
+ * The default system implementation can be accessed at {@link Clock#SYSTEM}.
+ * Primarily used for testing.
+ * </p>
+ *
+ * @since 1.9
+ * @author mlinder@google.com (Matthias Linder)
+ */
+public interface Clock {
+  /**
+   * Returns the current time in milliseconds since midnight, January 1, 1970 UTC, to match the
+   * behavior of {@link System#currentTimeMillis()}.
+   */
+  long currentTimeMillis();
+
+  /**
+   * Provides the default System implementation of a Clock by using
+   * {@link System#currentTimeMillis()}.
+   */
+  Clock SYSTEM = new Clock() {
+    public long currentTimeMillis() {
+      return System.currentTimeMillis();
+    }};
+}
