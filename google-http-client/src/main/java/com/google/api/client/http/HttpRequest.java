698f338312ce04caaaa34be336798ee6699e90fc
Ravi Mistry <rmistry@google.com>
2012-05-14 14:47:25 -0400

    Start next minor version

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -40,7 +40,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.9.0-beta-SNAPSHOT";
+  public static final String VERSION = "1.10.0-beta-SNAPSHOT";
 
   /**
    * User agent suffix for all requests.
