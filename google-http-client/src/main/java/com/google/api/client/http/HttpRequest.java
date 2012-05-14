01ee5aee0f16974024aa91e9f9472e05b61a5429
Ravi Mistry <rmistry@google.com>
2012-05-14 15:11:03 -0400

    start 1.9.1-beta

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -40,7 +40,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.9.0-beta";
+  public static final String VERSION = "1.9.1-beta-SNAPSHOT";
 
   /**
    * User agent suffix for all requests.
