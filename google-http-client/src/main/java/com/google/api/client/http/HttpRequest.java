4babd1b56f0154b92873d795a043ec40fb87a4f9
Nick Miceli <ngmiceli@google.com>
2012-12-21 12:10:04 -0500

    http: Start 1.13.2-beta

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -45,7 +45,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.13.1-beta";
+  public static final String VERSION = "1.13.2-beta-SNAPSHOT";
 
   /**
    * User agent suffix for all requests.
