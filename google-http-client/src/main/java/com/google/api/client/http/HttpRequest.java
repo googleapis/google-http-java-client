15d79631a6419f43f0640d3723552b5f01009305
Nick Miceli <ngmiceli@google.com>
2013-02-26 15:45:11 -0500

    http: Update default branch to 1.15.0-beta https://codereview.appspot.com/7387052/

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -50,7 +50,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.14.0-beta-SNAPSHOT";
+  public static final String VERSION = "1.15.0-beta-SNAPSHOT";
 
   /**
    * User agent suffix for all requests.
