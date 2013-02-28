d20582cd5b436c8742ad4f8d613c34e508dcc719
Yaniv Inbar <yanivi@google.com>
2013-02-28 14:55:05 -0500

    http: add a period to avoid compile warning.
    https://codereview.appspot.com/7438046/

--- google-http-client/src/main/java/com/google/api/client/util/Collections2.java
+++ google-http-client/src/main/java/com/google/api/client/util/Collections2.java
@@ -28,9 +28,7 @@ import java.util.Collection;
  */
 public final class Collections2 {
 
-  /**
-   * Used to avoid http://bugs.sun.com/view_bug.do?bug_id=6558557
-   */
+  /** Used to avoid http://bugs.sun.com/view_bug.do?bug_id=6558557. */
   static <T> Collection<T> cast(Iterable<T> iterable) {
     return (Collection<T>) iterable;
   }
