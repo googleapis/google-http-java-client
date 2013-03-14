2342fcbeb718b8b801a7a4409fd82c65edba04ed
Nick Miceli <ngmiceli@google.com>
2013-03-14 17:07:47 -0400

    http 1.14: Prepare branch for release https://codereview.appspot.com/7820044/

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -48,7 +48,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.14.0-beta-SNAPSHOT";
+  public static final String VERSION = "1.14.0-beta";
 
   /**
    * User agent suffix for all requests.
