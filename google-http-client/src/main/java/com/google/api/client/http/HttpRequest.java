68b9bc2858e8bb1cf6450a2e5428024a14af1dab
Yaniv Inbar <yanivi@google.com>
2013-04-17 11:16:29 -0400

    http: beta -> rc (release candidate)
    https://codereview.appspot.com/8762048/

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -49,7 +49,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.15.0-beta-SNAPSHOT";
+  public static final String VERSION = "1.15.0-rc-SNAPSHOT";
 
   /**
    * User agent suffix for all requests.
