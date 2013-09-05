82b18d4c5c8c375062ae32e348fbf4af9cc4d4ae
Nick Miceli <ngmiceli@google.com>
2013-09-05 13:47:18 -0400

    http 1.17: Start next bugfix version https://codereview.appspot.com/13566043/

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -50,7 +50,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.17.0-rc";
+  public static final String VERSION = "1.17.1-rc-SNAPSHOT";
 
   /**
    * User agent suffix for all requests.
