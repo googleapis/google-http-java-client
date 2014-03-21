83c99bd776d991832da6689918042f81987b11f9
Daniel Wang <wonderfly.google.com>
2014-03-21 13:48:54 -0700

    http 1.18: Start next bugfix version

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -50,7 +50,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.18.0-rc";
+  public static final String VERSION = "1.18.1-rc-SNAPSHOT";
 
   /**
    * User agent suffix for all requests.
