3e48f933ca8ce38d5014b123471bc8dbe51828a2
Daniel Wang <wonderfly@google.com>
2014-07-16 17:06:42 -0700

    Start 1.20.0 version

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -50,7 +50,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.19.0-rc-SNAPSHOT";
+  public static final String VERSION = "1.20.0-SNAPSHOT";
 
   /**
    * User agent suffix for all requests.
