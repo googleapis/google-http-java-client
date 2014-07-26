5626cc12bc474592e4ee36ec3a2aa3f5e9d4047d
Daniel Wang <wonderfly.google.com>
2014-07-26 11:25:08 -0700

    http samples: Update dependencies.

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -50,7 +50,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.19.0";
+  public static final String VERSION = "1.19.1-SNAPSHOT";
 
   /**
    * User agent suffix for all requests.
