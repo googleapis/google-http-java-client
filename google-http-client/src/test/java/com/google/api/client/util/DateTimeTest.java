57b3e74eca8317d434223b63bd85a3b01078fa1c
emrekultursay <emrekultursay@google.com>
2015-10-07 18:43:35 -0700

    Fix milliseconds bug in DateTime parseRfc3339. The fraction after the dot is not always 3 digits, in which case it must be multiplied by 10^n to get the correct milliseconds.
    Example1: 12:34.56 should have milliseconds=560, but it was returning only 56.
    
    Example2: 12:34.56789 should have milliseconds=567, but it was returning 56789.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=104921033

--- google-http-client/src/test/java/com/google/api/client/util/DateTimeTest.java
+++ google-http-client/src/test/java/com/google/api/client/util/DateTimeTest.java
@@ -120,7 +120,7 @@ public class DateTimeTest extends TestCase {
     assertEquals(0, value.getValue() % 100);
     value = DateTime.parseRfc3339("1997-01-01T12:00:27.87+00:20");
     assertFalse(value.isDateOnly());
-    assertEquals(87, value.getValue() % 1000); // check milliseconds
+    assertEquals(870, value.getValue() % 1000); // check milliseconds
 
     value = new DateTime("2007-06-01");
     assertTrue(value.isDateOnly());
@@ -136,6 +136,22 @@ public class DateTimeTest extends TestCase {
         DateTime.parseRfc3339("1990-12-31T15:59:60-08:00").getValue()); // from Section 5.8 Examples
     assertEquals(DateTime.parseRfc3339("2007-06-01t18:50:00-04:00").getValue(),
         DateTime.parseRfc3339("2007-06-01t22:50:00Z").getValue()); // from Section 4.2 Local Offsets
+  }
+
+  public void testParseAndFormatRfc3339() {
+    // .12 becomes .120
+    String input = "1996-12-19T16:39:57.12-08:00";
+    String expected = "1996-12-19T16:39:57.120-08:00";
+    DateTime dt = DateTime.parseRfc3339(input);
+    String output = dt.toStringRfc3339();
+    assertEquals(expected, output);
+
+    // Truncated to milliseconds.
+    input    = "1996-12-19T16:39:57.123456789-08:00";
+    expected = "1996-12-19T16:39:57.123-08:00";
+    dt = DateTime.parseRfc3339(input);
+    output = dt.toStringRfc3339();
+    assertEquals(expected, output);
   }
 
   private void expectExceptionForParseRfc3339(String input) {
