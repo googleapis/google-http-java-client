b69f50e2265e897b2830b983e03b37524eb1e290
neozwu <neozwu@users.noreply.github.com>
2017-09-13 15:56:36 -0700

    start 1.24 (#369)

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -50,7 +50,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.23.0-SNAPSHOT";
+  public static final String VERSION = "1.24.0-SNAPSHOT";
 
   /**
    * User agent suffix for all requests.
