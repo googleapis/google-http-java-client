3f9ee79ff52440539ab1384d5419f602b6f52ed1
Jeff Ching <chingor@google.com>
2018-10-17 15:53:35 -0700

    Fix straggler versions for assembly

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -50,7 +50,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.26.0-SNAPSHOT";
+  public static final String VERSION = "1.26.0";
 
   /**
    * User agent suffix for all requests.
