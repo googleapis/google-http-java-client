815840b3683e33ce607d16b0e0b269e656886281
Jeff Ching <chingor@google.com>
2018-11-09 10:02:20 -0800

    Fix line length

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -50,7 +50,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.27.0"; // {x-version-update:google-http-client-parent:current}
+  public static final String VERSION = "1.27.0";
 
   /**
    * User agent suffix for all requests.
