423db6987cc591a98c341754b72833e88e3f41ce
Jeff Ching <chingor@google.com>
2018-11-09 09:36:11 -0800

    Fix VERSION constant

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -50,7 +50,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.26.0";
+  public static final String VERSION = "1.27.0"; // {x-version-update:google-http-client-parent:current}
 
   /**
    * User agent suffix for all requests.
