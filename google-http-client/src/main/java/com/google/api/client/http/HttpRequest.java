2ee8e9958ef4e7854eea7c5d82218588aed27a2d
Jeff Ching <chingor@google.com>
2019-01-15 12:30:53 -0800

    Release google-http-java-client v1.28.0 (#574)
    * Release v1.28.0
    
    * Fix google-http-client-apache version for 2.0.0 release
    
    * Update VERSION constant

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -56,7 +56,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.27.0";
+  public static final String VERSION = "1.28.0";
 
   /**
    * User agent suffix for all requests.
