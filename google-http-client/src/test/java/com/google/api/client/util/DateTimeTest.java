1d496dc0a33d74b04b2616a5406ab9148ec3dfa0
Knut Olav Løite <koloite@gmail.com>
2019-07-31 16:12:11 +0200

    Add support for nanosecond precision when parsing rfc3339 strings (#752)
    * support nanosecond precision when parsing rfc3339 strings
    
    * removed hamcrest matchers
    
    * fix javadoc + change exception type
    
    * assert parse directly instead of using a map
    
    * put expected and actual in correct order

--- google-http-client/src/test/java/com/google/api/client/util/DateTimeTest.java
+++ google-http-client/src/test/java/com/google/api/client/util/DateTimeTest.java
@@ -14,6 +14,7 @@
 
 package com.google.api.client.util;
 
+import com.google.api.client.util.DateTime.SecondsAndNanos;
 import java.util.Date;
 import java.util.TimeZone;
 import junit.framework.TestCase;
@@ -142,6 +143,94 @@ public class DateTimeTest extends TestCase {
     assertEquals(
         DateTime.parseRfc3339("2007-06-01t18:50:00-04:00").getValue(),
         DateTime.parseRfc3339("2007-06-01t22:50:00Z").getValue()); // from Section 4.2 Local Offsets
+
+    // Test truncating beyond millisecond precision.
+    assertEquals(
+        DateTime.parseRfc3339(
+            "2018-12-31T23:59:59.999999999Z"), // This value would be rounded up prior to version
+        // 1.30.2
+        DateTime.parseRfc3339("2018-12-31T23:59:59.999Z"));
+    assertEquals(
+        DateTime.parseRfc3339(
+            "2018-12-31T23:59:59.9999Z"), // This value would be truncated prior to version 1.30.2
+        DateTime.parseRfc3339("2018-12-31T23:59:59.999Z"));
+  }
+
+  /**
+   * The following test values have been generated and verified using the {@link DateTimeFormatter}
+   * in Java 8.
+   *
+   * <pre>
+   * Timestamp                           |   Seconds     |   Nanos
+   * 2018-03-01T10:11:12.999Z            |   1519899072  |   999000000
+   * 2018-10-28T02:00:00+02:00           |   1540684800  |   0
+   * 2018-10-28T03:00:00+01:00           |   1540692000  |   0
+   * 2018-01-01T00:00:00.000000001Z      |   1514764800  |   1
+   * 2018-10-28T02:00:00Z                |   1540692000  |   0
+   * 2018-12-31T23:59:59.999999999Z      |   1546300799  |   999999999
+   * 2018-03-01T10:11:12.9999Z           |   1519899072  |   999900000
+   * 2018-03-01T10:11:12.000000001Z      |   1519899072  |   1
+   * 2018-03-01T10:11:12.100000000Z      |   1519899072  |   100000000
+   * 2018-03-01T10:11:12.100000001Z      |   1519899072  |   100000001
+   * 2018-03-01T10:11:12-10:00           |   1519935072  |   0
+   * 2018-03-01T10:11:12.999999999Z      |   1519899072  |   999999999
+   * 2018-03-01T10:11:12-12:00           |   1519942272  |   0
+   * 2018-10-28T03:00:00Z                |   1540695600  |   0
+   * 2018-10-28T02:30:00Z                |   1540693800  |   0
+   * 2018-03-01T10:11:12.123Z            |   1519899072  |   123000000
+   * 2018-10-28T02:30:00+02:00           |   1540686600  |   0
+   * 2018-03-01T10:11:12.123456789Z      |   1519899072  |   123456789
+   * 2018-03-01T10:11:12.1000Z           |   1519899072  |   100000000
+   * </pre>
+   */
+  public void testParseRfc3339ToSecondsAndNanos() {
+    assertParsedRfc3339(
+        "2018-03-01T10:11:12.999Z", SecondsAndNanos.ofSecondsAndNanos(1519899072L, 999000000));
+    assertParsedRfc3339(
+        "2018-10-28T02:00:00+02:00", SecondsAndNanos.ofSecondsAndNanos(1540684800L, 0));
+    assertParsedRfc3339(
+        "2018-10-28T03:00:00+01:00", SecondsAndNanos.ofSecondsAndNanos(1540692000L, 0));
+    assertParsedRfc3339(
+        "2018-01-01T00:00:00.000000001Z", SecondsAndNanos.ofSecondsAndNanos(1514764800L, 1));
+    assertParsedRfc3339("2018-10-28T02:00:00Z", SecondsAndNanos.ofSecondsAndNanos(1540692000L, 0));
+    assertParsedRfc3339(
+        "2018-12-31T23:59:59.999999999Z",
+        SecondsAndNanos.ofSecondsAndNanos(1546300799L, 999999999));
+    assertParsedRfc3339(
+        "2018-03-01T10:11:12.9999Z", SecondsAndNanos.ofSecondsAndNanos(1519899072L, 999900000));
+    assertParsedRfc3339(
+        "2018-03-01T10:11:12.000000001Z", SecondsAndNanos.ofSecondsAndNanos(1519899072L, 1));
+    assertParsedRfc3339(
+        "2018-03-01T10:11:12.100000000Z",
+        SecondsAndNanos.ofSecondsAndNanos(1519899072L, 100000000));
+    assertParsedRfc3339(
+        "2018-03-01T10:11:12.100000001Z",
+        SecondsAndNanos.ofSecondsAndNanos(1519899072L, 100000001));
+    assertParsedRfc3339(
+        "2018-03-01T10:11:12-10:00", SecondsAndNanos.ofSecondsAndNanos(1519935072L, 0));
+    assertParsedRfc3339(
+        "2018-03-01T10:11:12.999999999Z",
+        SecondsAndNanos.ofSecondsAndNanos(1519899072L, 999999999));
+    assertParsedRfc3339(
+        "2018-03-01T10:11:12-12:00", SecondsAndNanos.ofSecondsAndNanos(1519942272L, 0));
+    assertParsedRfc3339("2018-10-28T03:00:00Z", SecondsAndNanos.ofSecondsAndNanos(1540695600L, 0));
+    assertParsedRfc3339("2018-10-28T02:30:00Z", SecondsAndNanos.ofSecondsAndNanos(1540693800L, 0));
+    assertParsedRfc3339(
+        "2018-03-01T10:11:12.123Z", SecondsAndNanos.ofSecondsAndNanos(1519899072L, 123000000));
+    assertParsedRfc3339(
+        "2018-10-28T02:30:00+02:00", SecondsAndNanos.ofSecondsAndNanos(1540686600L, 0));
+    assertParsedRfc3339(
+        "2018-03-01T10:11:12.123456789Z",
+        SecondsAndNanos.ofSecondsAndNanos(1519899072L, 123456789));
+    assertParsedRfc3339(
+        "2018-03-01T10:11:12.1000Z", SecondsAndNanos.ofSecondsAndNanos(1519899072L, 100000000));
+  }
+
+  private void assertParsedRfc3339(String input, SecondsAndNanos expected) {
+    SecondsAndNanos actual = DateTime.parseRfc3339ToSecondsAndNanos(input);
+    assertEquals(
+        "Seconds for " + input + " do not match", expected.getSeconds(), actual.getSeconds());
+    assertEquals("Nanos for " + input + " do not match", expected.getNanos(), actual.getNanos());
   }
 
   public void testParseAndFormatRfc3339() {
