9f72095033f531a0e007b2a68bb66310b500c2f0
Jeff Ching <chingor@google.com>
2019-08-30 10:43:12 -0600

    fix: grab version from the package metadata

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -53,7 +53,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.30.0";
+  public static final String VERSION = getVersion();
 
   /**
    * User agent suffix for all requests.
@@ -1200,5 +1200,9 @@ public final class HttpRequest {
     if (value != null) {
       span.putAttribute(key, AttributeValue.stringAttributeValue(value));
     }
+  }
+  
+  private static String getVersion() {
+    return HttpRequest.class.getPackage().getImplementationVersion();
   }
 }
