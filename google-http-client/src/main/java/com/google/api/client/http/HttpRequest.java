d82718d0cc2f75ec9fd5db1b32f80d02e5c365a0
Jeff Ching <chingor@google.com>
2019-09-04 12:45:25 -0700

    fix: grab version from the package metadata (#806)
    * fix: grab version from the package metadata
    
    * test: ensure VERSION constant matches semver pattern
    
    * fix: use version from manifest, fallback to generated properties file

--- google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
+++ google-http-client/src/main/java/com/google/api/client/http/HttpRequest.java
@@ -30,6 +30,7 @@ import io.opencensus.trace.Span;
 import io.opencensus.trace.Tracer;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -53,7 +54,7 @@ public final class HttpRequest {
    *
    * @since 1.8
    */
-  public static final String VERSION = "1.30.0";
+  public static final String VERSION = getVersion();
 
   /**
    * User agent suffix for all requests.
@@ -1200,5 +1201,23 @@ public final class HttpRequest {
     if (value != null) {
       span.putAttribute(key, AttributeValue.stringAttributeValue(value));
     }
+  }
+  
+  private static String getVersion() {
+    String version = HttpRequest.class.getPackage().getImplementationVersion();
+    // in a non-packaged environment (local), there's no implementation version to read
+    if (version == null) {
+      // fall back to reading from a properties file - note this value is expected to be cached
+      try (InputStream inputStream = HttpRequest.class.getResourceAsStream("/google-http-client.properties")) {
+        if (inputStream != null) {
+          Properties properties = new Properties();
+          properties.load(inputStream);
+          version = properties.getProperty("google-http-client.version");
+        }
+      } catch (IOException e) {
+        // ignore
+      }
+    }
+    return version;
   }
 }
