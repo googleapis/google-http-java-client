dd3921c65bd1909140d367b4958f47feeb7a0462
Elliotte Rusty Harold <elharo@ibiblio.org>
2020-01-27 13:59:19 -0500

    unused import

--- google-http-client/src/test/java/com/google/api/client/http/ConsumingInputStreamTest.java
+++ google-http-client/src/test/java/com/google/api/client/http/ConsumingInputStreamTest.java
@@ -20,7 +20,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import org.junit.Test;
 
