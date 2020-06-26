05160aa10839e9ca5502f8f061089fe5c1bc40ea
Jeff Ching <chingor@google.com>
2020-06-26 14:27:15 -0700

    samples: replace Google+ json example with YouTube example (#1069)
    * samples: add json sample parsing example from YouTube search API
    
    * samples: add support for Map type
    
    * samples(docs): update json parsing example
    
    * samples: change some example data
    
    * samples: fix missing brace
    
    * samples: fix missing 'public static'

---
+++ samples/snippets/src/main/java/com/example/json/YouTubeSample.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.json;
+
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.util.Key;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class YouTubeSample {
+  public static class ListResponse {
+    @Key("items")
+    private List<SearchResult> searchResults;
+
+    @Key
+    private PageInfo pageInfo;
+
+    public List<SearchResult> getSearchResults() {
+      return searchResults;
+    }
+
+    public PageInfo getPageInfo() {
+      return pageInfo;
+    }
+  }
+
+  public static class PageInfo {
+    @Key
+    private long totalResults;
+
+    @Key
+    private long resultsPerPage;
+
+    public long getTotalResults() {
+      return totalResults;
+    }
+
+    public long getResultsPerPage() {
+      return resultsPerPage;
+    }
+  }
+
+  public static class SearchResult {
+    @Key
+    private String kind;
+
+    @Key("id")
+    private VideoId videoId;
+
+    @Key
+    private Snippet snippet;
+
+    public String getKind() {
+      return kind;
+    }
+
+    public VideoId getId() {
+      return videoId;
+    }
+
+    public Snippet getSnippet() {
+      return snippet;
+    }
+  }
+
+  public static class VideoId {
+    @Key
+    private String kind;
+
+    @Key
+    private String videoId;
+
+    public String getKind() {
+      return kind;
+    }
+
+    public String getVideoId() {
+      return videoId;
+    }
+  }
+
+  public static class Snippet {
+    @Key
+    private String publishedAt;
+
+    @Key
+    private String channelId;
+
+    @Key
+    private String title;
+
+    @Key
+    private String description;
+
+    @Key
+    private Map<String, Thumbnail> thumbnails;
+
+    public String getPublishedAt() {
+      return publishedAt;
+    }
+
+    public String getChannelId() {
+      return channelId;
+    }
+
+    public String getTitle() {
+      return title;
+    }
+
+    public String getDescription() {
+      return description;
+    }
+
+    public Map<String, Thumbnail> getThumbnails() {
+      return thumbnails;
+    }
+  }
+
+  public static class Thumbnail {
+    @Key
+    private String url;
+
+    @Key
+    private long width;
+
+    @Key
+    private long height;
+
+    public String getUrl() {
+      return url;
+    }
+
+    public long getWidth() {
+      return width;
+    }
+
+    public long getHeight() {
+      return height;
+    }
+  }
+
+  public static ListResponse parseJson(HttpResponse httpResponse) throws IOException {
+    ListResponse listResponse = httpResponse.parseAs(ListResponse.class);
+    if (listResponse.getSearchResults().isEmpty()) {
+      System.out.println("No results found.");
+    } else {
+      for (SearchResult searchResult : listResponse.getSearchResults()) {
+        System.out.println();
+        System.out.println("-----------------------------------------------");
+        System.out.println("Kind: " + searchResult.getKind());
+        System.out.println("Video ID: " + searchResult.getId().getVideoId());
+        System.out.println("Title: " + searchResult.getSnippet().getTitle());
+        System.out.println("Description: " + searchResult.getSnippet().getDescription());
+      }
+    }
+    return listResponse;
+  }
+
+}
---
+++ samples/snippets/src/test/java/com/example/json/YouTubeSampleTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.json;
+
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.LowLevelHttpRequest;
+import com.google.api.client.http.LowLevelHttpResponse;
+import com.google.api.client.json.Json;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.testing.http.HttpTesting;
+import com.google.api.client.testing.http.MockHttpTransport;
+import com.google.api.client.testing.http.MockLowLevelHttpRequest;
+import com.google.api.client.testing.http.MockLowLevelHttpResponse;
+import com.google.common.base.Preconditions;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class YouTubeSampleTest {
+
+  @Test
+  public void testParsing() throws IOException {
+    final InputStream contents = getClass().getClassLoader().getResourceAsStream("youtube-search.json");
+    Preconditions.checkNotNull(contents);
+    HttpTransport transport =
+        new MockHttpTransport() {
+          @Override
+          public LowLevelHttpRequest buildRequest(String method, String url) throws IOException {
+            return new MockLowLevelHttpRequest() {
+              @Override
+              public LowLevelHttpResponse execute() throws IOException {
+                MockLowLevelHttpResponse result = new MockLowLevelHttpResponse();
+                result.setContentType(Json.MEDIA_TYPE);
+                result.setContent(contents);
+                return result;
+              }
+            };
+          }
+        };
+    HttpRequest request =
+        transport.createRequestFactory().buildGetRequest(HttpTesting.SIMPLE_GENERIC_URL);
+    request.setParser(new JsonObjectParser(new GsonFactory()));
+    HttpResponse response = request.execute();
+
+    YouTubeSample.ListResponse listResponse = YouTubeSample.parseJson(response);
+    assertEquals(5, listResponse.getPageInfo().getResultsPerPage());
+    assertEquals(1000000, listResponse.getPageInfo().getTotalResults());
+    assertEquals(5, listResponse.getSearchResults().size());
+    for (YouTubeSample.SearchResult searchResult : listResponse.getSearchResults()) {
+      assertEquals("youtube#searchResult", searchResult.getKind());
+      assertNotNull(searchResult.getId());
+      assertEquals("youtube#video", searchResult.getId().getKind());
+      assertNotNull(searchResult.getId().getVideoId());
+      YouTubeSample.Snippet snippet = searchResult.getSnippet();
+      assertNotNull(snippet);
+      assertNotNull(snippet.getChannelId());
+      assertNotNull(snippet.getDescription());
+      assertNotNull(snippet.getTitle());
+      assertNotNull(snippet.getPublishedAt());
+      Map<String, YouTubeSample.Thumbnail> thumbnails = snippet.getThumbnails();
+      assertNotNull(thumbnails);
+
+      for (Map.Entry<String, YouTubeSample.Thumbnail> entry : thumbnails.entrySet()) {
+        assertNotNull(entry.getKey());
+        YouTubeSample.Thumbnail thumbnail = entry.getValue();
+        assertNotNull(thumbnail);
+        assertNotNull(thumbnail.getUrl());
+        assertNotNull(thumbnail.getWidth());
+        assertNotNull(thumbnail.getHeight());
+      }
+    }
+  }
+}
