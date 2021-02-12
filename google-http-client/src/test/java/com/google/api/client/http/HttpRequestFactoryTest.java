9a02c9239824e5012daf331ccf303c45eb2b4079
arithmetic1728 <liusijun1985@gmail.com>
2021-02-11 16:45:13 -0800

    chore: add unit test

---
+++ google-http-client/src/test/java/com/google/api/client/http/HttpRequestFactoryTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.api.client.http;
+
+import com.google.api.client.http.javanet.NetHttpTransport;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests {@link HttpRequestFactory}.
+ */
+public class HttpRequestFactoryTest extends TestCase {
+
+  public void testBuildRequest_urlShouldBeSet() throws Exception {
+    HttpRequestFactory requestFactory =
+        new NetHttpTransport().createRequestFactory(
+            new HttpRequestInitializer() {
+              @Override
+              public void initialize(HttpRequest request) {
+                // Url should be set by buildRequest method before calling initialize. 
+                if (request.getUrl() == null) {
+                    throw new IllegalArgumentException("url is not set in request");
+                }
+              }
+            });
+    GenericUrl url = new GenericUrl("https://foo.googleapis.com/");
+    HttpRequest request = requestFactory.buildRequest("GET", url, null);
+    assertEquals(url, request.getUrl());
+  }
+}
