0042ea46116fcccae5bb3bd771d1c947dca99a5c
Chanseok Oh <chanseok@google.com>
2021-10-08 15:53:27 -0400

    Update google-http-client/src/main/java/com/google/api/client/http/GZipEncoding.java

--- google-http-client/src/main/java/com/google/api/client/http/GZipEncoding.java
+++ google-http-client/src/main/java/com/google/api/client/http/GZipEncoding.java
@@ -43,7 +43,7 @@ public class GZipEncoding implements HttpEncoding {
             try {
               flush();
             } catch (IOException ignored) {
-                // fall through
+              // fall through
             }
           }
         };
