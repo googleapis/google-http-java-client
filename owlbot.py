7c28ac6d959138117c8e9a87c865579b899a625d
Alice Li <alicejli@google.com>
2024-03-22 12:45:02 -0400

    remove ci.yaml from owlbot post-processing

--- owlbot.py
+++ owlbot.py
@@ -31,7 +31,7 @@ java.common_templates(
         ".github/workflows/samples.yaml",
         ".kokoro/build.sh",
         "renovate.json",
-        ".github/workflows/ci.yaml"
+        ".github/workflows/ci.yaml",
         ".kokoro/requirements.in",
         ".kokoro/requirements.txt"
     ]
