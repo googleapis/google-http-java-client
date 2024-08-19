ff5c20993f5c8e366ed5538b26c61a87b4409cb1
Diego Alonso Marquez Palacios <diegomarquezp@google.com>
2024-08-19 17:20:31 -0400

    add javadoc for Apache5ResponseContent

--- google-http-client-apache-v5/src/main/java/com/google/api/client/http/apache/v5/Apache5ResponseContent.java
+++ google-http-client-apache-v5/src/main/java/com/google/api/client/http/apache/v5/Apache5ResponseContent.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import org.apache.hc.core5.http.ClassicHttpResponse;
 
+/**
+ * Class that wraps an {@link org.apache.hc.core5.http.HttpEntity}'s content {@link InputStream} along with the
+ * {@link ClassicHttpResponse} that contains this entity. The main purpose is to be able to close the response
+ * as well as the content input stream when {@link #close()} is called.
+ */
 public class Apache5ResponseContent extends InputStream {
   private final ClassicHttpResponse response;
   private final InputStream wrappedStream;
