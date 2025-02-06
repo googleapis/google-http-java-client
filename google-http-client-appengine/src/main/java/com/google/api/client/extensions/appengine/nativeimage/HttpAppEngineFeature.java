32e38d64eb9c1be6858787e4719797d09c4a83e3
diegomarquezp <diegomarquezp@google.com>
2025-02-06 16:04:34 +0000

    Revert "try setup time feature registration"
    This reverts commit eeb9352a031564eb02ef52eab0f6cc4a3b296f5f.

--- google-http-client-appengine/src/main/java/com/google/api/client/extensions/appengine/nativeimage/HttpAppEngineFeature.java
+++ google-http-client-appengine/src/main/java/com/google/api/client/extensions/appengine/nativeimage/HttpAppEngineFeature.java
@@ -18,7 +18,7 @@ public class HttpAppEngineFeature implements Feature {
           "com.google.apphosting.api.logservice.LogServicePb");
 
   @Override
-  public void duringSetup(DuringSetupAccess access) {
+  public void beforeAnalysis(BeforeAnalysisAccess access) {
     try {
       for (String className : APP_ENGINE_CLASSES) {
         registerClassForReflection(className, access);
@@ -31,7 +31,8 @@ public class HttpAppEngineFeature implements Feature {
     }
   }
 
-  private void registerClassForReflection(String classFullQualifiedName, DuringSetupAccess access) {
+  private void registerClassForReflection(
+      String classFullQualifiedName, BeforeAnalysisAccess access) {
     Class<?> clazz = access.findClassByName(classFullQualifiedName);
     RuntimeReflection.register(clazz);
     RuntimeReflection.register(clazz.getDeclaredConstructors());
